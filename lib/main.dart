80461df798d3e115a208200ddb6aea41e0eb4679
galangkrisnanto <galangkrsnt@gmail.com>
2023-02-08 14:46:04 +0700

    [ADD] Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp>{
+  var questionIndex =0;
+
+  void answerQuestion(){
+    setState(() {
+      questionIndex = questionIndex +1;
+    });
+    }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color ?',
+      'What\'s your favourite animal ?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -14,17 +35,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The questions!'),
+            Question(questions[questionIndex]),
             ElevatedButton(
-              onPressed: null,
+              onPressed: answerQuestion,
               child: Text('Answer 1'),
             ),
             ElevatedButton(
-              onPressed: null,
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue),),
+              onPressed: answerQuestion,
               child: Text('Answer 2'),
             ),
             ElevatedButton(
-              onPressed: null,
+              onPressed: answerQuestion,
               child: Text('Answer 3'),
             ),
           ],
