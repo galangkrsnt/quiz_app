6a2411042464d51baf2a943bf94b28c08336a390
galangkrisnanto <galangkrsnt@gmail.com>
2023-02-13 13:51:56 +0700

    [FIX] split widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './question.dart';
+import '../quiz.dart';
+import '../result.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -13,44 +15,39 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class MyAppState extends State<MyApp>{
-  var questionIndex =0;
+class MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite color ?',
+      'answers': ['Black', 'Blue', 'White', 'Green', 'Yellow'],
+    },
+    {
+      'questionText': 'What\'s your favourite animal ?',
+      'answers': ['Tiger', 'Dog', 'Cat', 'Fish', 'Frog'],
+    },
+    {
+      'questionText': 'What\'s your favourite food ?',
+      'answers': ['Noodle', 'Fried Rice', 'Chicken'],
+    },
+  ];
 
-  void answerQuestion(){
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex +1;
+      _questionIndex = _questionIndex + 1;
     });
-    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favourite color ?',
-      'What\'s your favourite animal ?',
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(questions[questionIndex]),
-            ElevatedButton(
-              onPressed: answerQuestion,
-              child: Text('Answer 1'),
-            ),
-            ElevatedButton(
-              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue),),
-              onPressed: answerQuestion,
-              child: Text('Answer 2'),
-            ),
-            ElevatedButton(
-              onPressed: answerQuestion,
-              child: Text('Answer 3'),
-            ),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(questions: _questions, questionIndex: _questionIndex,answerQuestion: _answerQuestion)
+            : Result()
       ),
     );
   }
